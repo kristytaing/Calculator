27d959f5799ce776e41d7a2b8370d5e640e0174c
kristytaing <ktaing@asu.edu>
2019-02-23 15:53:57 -0700

    Add JavaDoc Documentation

---
+++ Calculator.java
@@ -0,0 +1,64 @@
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/*
+	 * Returns the current total
+	 * 
+	 * @return the current total 
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/*
+	 * Adds the parameter to the total
+	 * 
+	 * @param value amount to be added to the total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/*
+	 * Subtracts a value from the total
+	 * 
+	 * @param value amount to be added to the total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/*
+	 * Multiplies the total by the parameter
+	 * 
+	 * @param value amount to be added to the total
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/*
+	 * Divides the total by the parameter
+	 * 
+	 * @param value amount to be added to the total
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/*
+	 * Returns a history of all actions as a string
+	 * 
+	 * @return a history of all actions 
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
